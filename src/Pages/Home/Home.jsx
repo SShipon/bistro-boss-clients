92e39474aedf811cd03cb07bf11905c746793e66
SShipon <nxshipon@gmail.com>
2023-08-17 14:34:54 +0600

    bistro boss add

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomeCom from '../../components/Home/Home';
+const Home = () => {
+    return (
+        <div>
+           <HomeCom></HomeCom>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+             <h2>this is hello i shipon</h2>
+        </div>
+    );
+};
+
+export default Home;
